f924c2440f87fc19dc8b1cc7617a10b2a05c6948
Seriki Walter Yakub <webapp4@outlook.com>
2026-08-07 23:10:36 +0100

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,3 +1,7 @@
 ssh git@git.qubu.ai
 git clone git@git.qubu.ai:<auraecosystem>/<dataset>.git
 cd dataset
+codex --model gpt-5.5 \
+  -c model_provider=exe-lmlm \
+  -c 'model_providers.exe-lmlm.name="exe-lmlm"' \
+  -c 'model_providers.exe-lmlm.base_url="https://lmlm.int.exe.xyz/v1"'
